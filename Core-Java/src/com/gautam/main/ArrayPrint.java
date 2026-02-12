e21331ef82aab55281975ee0d89d24b68120a1b4
Gautam Kumar <gautam14cs@gmail.com>
2026-02-12 23:10:34 +0530

    Initial commit

---
+++ Core-Java/src/com/gautam/main/ArrayPrint.java
@@ -0,0 +1,12 @@
+package com.gautam.main;
+
+import java.util.Arrays;
+
+public class ArrayPrint {
+	public static void main(String[] args) {
+		
+		int[] arr = {11,23,33,44,55,66,77,88,99,100};
+		
+		System.out.println(" Original Array Data: "+Arrays.toString(arr));
+	}
+}
---
+++ Core-Java/src/com/gautam/model/Address.java
@@ -0,0 +1,97 @@
+package com.gautam.model;
+
+public class Address {
+	
+	private String city; 
+	private String state; 
+	private int pinCode; 
+	private String country;
+	
+	public Address(String city, String state, int pinCode, String country) {
+		this.city = city; 
+		this.state = state; 
+		this.pinCode = pinCode;
+		this.country = country;
+	}
+	
+//	public Address(Address address) {
+//		this(address.getCity(), address.getState(), address.getPinCode(), address.getCountry());
+//	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+	
+	public String getCountry( ) {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + pinCode;
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (pinCode != other.pinCode)
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", state=" + state + ", pinCode=" + pinCode + ", Country= "+country+"]";
+	}
+	
+}
